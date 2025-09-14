8127999bbe301813f6637e7fd7900033261be2b2
ayushzjzjjjzk <sarladive140@gmail.com>
2025-09-14 14:36:09 +0800

    ffirst commit

---
+++ components/ui/dashboard/Sjs.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Sjs = () => {
+  return (
+    <div className='bg-blue-600'>
+      <h1>ayush singh  ramola </h1>
+      
+    </div>
+  )
+}
+
+export default Sjs
